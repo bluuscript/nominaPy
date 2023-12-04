76ed93aeb3190c4a47475ef23b3583e4f33c0660
Tomás Fuentes <tomymilla22@gmail.com>
2023-12-03 21:04:05 -0300

    env settings and added gunicorn

--- nominaApp/settings.py
+++ nominaApp/settings.py
@@ -21,12 +21,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
-SECRET_KEY = os.environ.get('SECRET_KEY', default='secret-unsafe')
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DEBUG')
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS').split(' ')
 
 # Session config
 SESSION_EXPIRE_SECONDS = 1800  # Expire after 30 minutes
